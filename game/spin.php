a00342596097316c6365ae95c95487ccf814228a
yenertuz <yenertuz@hotmail.com>
2019-03-27 15:23:12 -0700

    add spin.php

---
+++ game/spin.php
@@ -0,0 +1,43 @@
+<?php
+
+function check_post() {
+    if (isset($_POST["room_name"]) == false) {
+        $error = [];
+        $error["error"] = "missing_post_keys";
+        echo json_encode($error);
+        exit();
+    }
+}
+
+function check_if_the_room_has_enough_people($database, $room_id) {
+    $query = $database->prepare()
+}
+
+header("Access-Control-Allow-Origin: *");
+
+$database = new SQLite3($_SERVER["DOCUMENT_ROOT"] . "/yenertuz");
+
+check_post();
+
+check_if_room_has_enough_people($database, $_POST["room_id"]);
+
+$asker_user_object = get_asker_id_and_increment_asked_count($database, $_POST["room_name"]);
+$asker_id = $asker_user_object["id"];
+$asker_user_name = $asker_user_object["name"];
+$replier_user_object = get_replier_id_and_increment_replied_count($database, $_POST["room_name"], $asker_id, $asker_user_name);
+$replier_id = $replier_user_object["id"];
+$replier_user_name = $replier_user_object["name"];
+
+$room_description = update_room_status_and_asker_id_and_replier_id($database, $_POST["room_name"], $asker_id, $replier_id);
+
+$to_echo = [];
+$to_echo["asker_user_name"] = $asker_user_name;
+$to_echo["replier_user_name"] = $replier_user_name;
+$to_echo["room_status"] = "waiting_for_choice";
+$to_echo["room_description"] = $room_description;
+
+echo json_encode($to_echo);
+
+$database->close
+
+?>
