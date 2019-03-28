7b443cbe19d93d570caa64a38a06eef9fcfd5369
yenertuz <yenertuz@hotmail.com>
2019-03-27 22:38:38 -0700

    update rooms/create.php

--- game/sse.php
+++ game/sse.php
@@ -4,6 +4,10 @@ header("Access-Control-Allow-Origin: *");
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 
+if (isset($_GET["room_id"]) == false) {
+  exit();
+}
+
 $room_id = $_GET["room_id"];
 $database = new SQLite3($_SERVER["DOCUMENT_ROOT"] . "/yenertuz");
 $query = $database->prepare("select * from rooms where id = :id");
--- rooms/create.php
+++ rooms/create.php
@@ -4,8 +4,7 @@ function check_post() {
   if (isset($_POST["creator_id"]) == false || 
       isset($_POST["name"]) == false ||
       isset($_POST["nickname"]) == false) {
-	  echo "Username not set";
-	  file_put_contents("log", "here1");
+	  echo json_encode(["error" => "bad_post"]);
       return(-1);
   }
   return (0);
@@ -17,7 +16,7 @@ function check_if_room_name_exists($database, $room_name) {
   $result = $query->execute();
   $count = $result->fetchArray(SQLITE3_NUM)[0];
   if ($count != 0) {
-	  $error["error"] = "Room name taken";
+	  $error["error"] = "room_full";
 	echo json_encode($error);
     return(-1);
   }
@@ -25,7 +24,7 @@ function check_if_room_name_exists($database, $room_name) {
 }
 
 function create_room_and_assign_creator_and_member_count_and_replier_id($database, $name, $creator_id, $creator_name) {
-  $query = $database->prepare("insert into rooms (name, creator_id, member_count, replier_user_name) values ( :name, :creator_id, 1, :replier_user_name)");
+  $query = $database->prepare("insert into rooms (name, creator_id, member_count, asker_user_name) values ( :name, :creator_id, 1, :replier_user_name)");
   $query->bindValue(":name", $name);
   $query->bindValue(":creator_id", $creator_id);
   $query->bindValue(":replier_user_name", $creator_name);
@@ -47,15 +46,27 @@ $database = new SQLite3($_SERVER["DOCUMENT_ROOT"] . "/yenertuz");
 
 $status = check_post();
 
+file_put_contents("log", $status);
+
 if ($status == 0) {
 
-	$status = check_if_room_name_exists($database, $_POST["name"]);
+  file_put_contents("log", "done&&");
+
+  $status = check_if_room_name_exists($database, $_POST["name"]);
+  
+  file_put_contents("log", "done2");
 
 	if ($status == 0) {
 
-		$room_id = create_room_and_assign_creator_and_member_count_and_replier_id($database, $_POST["name"], $_POST["creator_id"], $_POST["name"]);
+    $room_id = create_room_and_assign_creator_and_member_count_and_replier_id($database, $_POST["name"], $_POST["creator_id"], $_POST["nickname"]);
+    
+    file_put_contents("log", "done3");
 
-		update_user_and_assign_room_and_name($database, $_POST["creator_id"], $_POST["nickname"], $room_id);
+    update_user_and_assign_room_and_name($database, $_POST["creator_id"], $_POST["nickname"], $room_id);
+
+    file_put_contents("log", "done4");
+    
+
 
 		echo $room_id;
 
