c736018ade445e1447027a8049a1f1980cd5bde0
yenertuz <yenertuz@hotmail.com>
2019-03-27 20:49:43 -0700

    update game/sse.php

--- game/sse.php
+++ game/sse.php
@@ -1,8 +1,8 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
-header("Access-Control-Allow-Origin: *");
 
 $room_id = $_GET["room_id"];
 $database = new SQLite3($_SERVER["DOCUMENT_ROOT"] . "/yenertuz");
@@ -11,7 +11,7 @@ $query->bindValue(":id", $room_id);
 $result = $query->execute();
 $result_object = $result->fetchArray(SQLITE3_ASSOC);
 
-echo json_encode($result_object);
+echo "data: " . json_encode($result_object) . "\n\n";
 
 flush();
 
